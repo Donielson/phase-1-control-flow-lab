ac50c53131fbfc8674dfbe00bd6a05300a391cd7
Daniel Castro <dcastro91588@yahoo.com>
2022-07-01 10:28:33 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  //let ride = 600;
+
+  if (ride <= 400) {
+    return 'This one is on me!'
+  }
+  else if (ride < 2500 && 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (ride > 2500) {
+    return 'No can do.'
+  };
+};
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  //const city = 'NYC'
+  
+  const cityMessage = (city === 'NYC') ? ('Ok, sounds good.') : ('No go.');
+  return cityMessage;
+
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+  case 'not as generous':
+    return 'Thank you.';
+  default: 
+    return 'Bye.'
+  }
+  
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
 }
